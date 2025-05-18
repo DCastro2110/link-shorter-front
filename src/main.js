5772e4de97f25f4f1b83cf3d4d173b36c6278944
Davi Castro dos Santos <davicastro.dev@gmail.com>
2025-05-18 15:41:57 -0300

    feat: customize result box

--- src/main.js
+++ src/main.js
@@ -6,46 +6,59 @@ const form = document.querySelector(".form");
 const input = document.querySelector(".link-input");
 const result = document.querySelector(".result");
 const btn = document.querySelector("button");
+const copyToClipboard = document.querySelector(".clipboard");
+const resultBox = document.querySelector(".result-box");
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
   try {
-    btn.innerHTML = "<div class='animation'></div>"
-  
+    btn.innerHTML = "<div class='animation'></div>";
+
     const inputValue = input.value;
-  
+
     if (input.value.trim() === "") {
       throw new Error("O campo não pode ser vazio!");
     }
-  
+
     const urlRegex = new RegExp(
       /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/
     );
-  
+
     if (!inputValue.match(urlRegex)) {
       throw new Error("URL inválida!");
     }
 
-     const res = await axios(
+    const res = await axios(
       `${import.meta.env.VITE_BASE_URL}/api?q=${inputValue}`,
       {
         method: "POST",
         timeout: 150000,
-        timeoutErrorMessage: "Tempo esgotado!"
+        timeoutErrorMessage: "Tempo esgotado!",
       }
     );
-    const { url } = res.data
+    const { url } = res.data;
 
-    if(res.status !== 200) {
-      throw new Error("Não foi possível gerar seu link. Tente novamente mais tarde");
+    if (res.status !== 200) {
+      throw new Error(
+        "Não foi possível gerar seu link. Tente novamente mais tarde"
+      );
     }
 
-    result.setAttribute("href", url);
+    resultBox.style.display = "inline-flex";
+    result.setAttribute("href", `https://${url}`);
     result.innerHTML = url;
-  } catch(err) {
-    alert(err.message)
+  } catch (err) {
+    alert(err.message);
   } finally {
-    btn.innerHTML = "Enviar"
+    btn.innerHTML = "Enviar";
   }
+});
 
+copyToClipboard.addEventListener("click", async () => {
+  try {
+    navigator.clipboard.writeText(result.textContent);
+    alert("Copiado com sucesso!");
+  } catch (err) {
+    alert("Erro ao copiar!");
+  }
 });
